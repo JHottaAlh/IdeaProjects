8728f86ccfcbbba01d905158dda56768c4d3003a
JHottaAlh <alhinc.hotta.jun@gmail.com>
2017-06-02 17:52:47 +0900

    Update user_control.sql
    rework

--- srcFolder/user_control.sql
+++ srcFolder/user_control.sql
@@ -1,11 +1,16 @@
-create view user_control as
+alter view user_control as
 select
-	id,
-    login_id,
-    name,
-	is_stopped,
-    password,
-    branch_id,
-    department_id
+	users.id
+	,login_id
+	,users.name
+	,is_stopped
+	,password
+	,branch_id
+	,department_id
+	,branches.name as branch_name
+	,departments.name as department_name
 from
-	users
+	users, branches, departments
+WHERE
+	users.branch_id = branches.id and
+	users.department_id = departments.id
